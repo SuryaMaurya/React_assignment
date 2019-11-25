3965fdabc556f9607a29437a2a5b975e08156a1b
SuryaMaurya <57386218+SuryaMaurya@users.noreply.github.com>
2019-11-25 11:07:50 +0530

    Add files via upload

--- src/Components/Dropdown.js
+++ src/Components/Dropdown.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
-class Dropdown extends React.Component {
+import '../App.css';
 
+class Dropdown extends React.Component {
+    
     render() {
         return (
             <div align='center'>
-                <p>{this.props.dropdown}</p>
-                <select>
-                    <option value={this.props.one}>{this.props.one}</option>
-                    <option value={this.props.two}>{this.props.two}</option>
-                    <option value={this.props.three}>{this.props.three}</option>
-                    <option value={this.props.four}>{this.props.four}</option>
+                {/* <p>{this.props.dropdown}</p> */}
+                <select id="select">                 
+                   {this.props.value.map(i => <option value={i}>{i}</option>)}
                 </select>
             </div>
         )
